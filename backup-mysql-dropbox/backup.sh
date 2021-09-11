f71e9e92e095bd8ea2984882d7671fe2d2bc0c01
Martín Lombardo <to@mlomb.me>
2021-09-10 22:38:16 -0300

    mysql backup working

--- backup-mysql-dropbox/backup.sh
+++ backup-mysql-dropbox/backup.sh
@@ -6,20 +6,21 @@ date
 echo "Backing up MySQL..."
 
 MYSQL_HOST_OPTS="-h $MYSQL_HOST -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD"
-databases=$(mysql $MYSQL_HOST_OPTS -e 'SHOW DATABASES;' --silent)
+databases=$DATABASES
 
 for database in $databases
 do
-  if [ "$database" != "information_schema" ] || [ "$database" != "performance_schema" ]
-  then
     echo "Database: $database"
     mysqldump $MYSQL_HOST_OPTS $MYSQLDUMP_OPTIONS $database \
       | gzip \
-      | curl -X PUT \
-             -H "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
-             -T - \
-             --output /dev/null \
-             https://content.dropboxapi.com/1/files_put/auto/$DROPBOX_FILEPATH.sql.gz
-             #--silent \
-  fi
+      | curl -X POST \
+        -H "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
+        -H "Dropbox-API-Arg: {\"path\": \"/$DROPBOX_FILEPATH.sql.gz\",\"mode\": \"overwrite\"}" \
+        -H "Content-Type: application/octet-stream" \
+        -T - \
+        --silent \
+        --output /dev/null \
+        https://content.dropboxapi.com/2/files/upload
 done
+
+echo "Done."
